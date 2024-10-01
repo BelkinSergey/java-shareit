5545519bbdd7b8339d17342ea061ba23ef2e8f4a
Sergey Belkin <belkinserega95@yandex.ru>
2024-10-01 11:29:29 +0300

    ShareIt версия2

--- src/main/java/ru/practicum/shareit/user/model/User.java
+++ src/main/java/ru/practicum/shareit/user/model/User.java
@@ -4,10 +4,6 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-
 @Getter
 @Setter
 @AllArgsConstructor
