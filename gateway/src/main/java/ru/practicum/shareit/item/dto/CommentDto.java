579f067bb8e74b177a082d25a6623c6bd366f9ab
Sergey Belkin <belkinserega95@yandex.ru>
2024-11-07 23:08:21 +0300

    Тз 16 исправлено

--- gateway/src/main/java/ru/practicum/shareit/item/dto/CommentDto.java
+++ gateway/src/main/java/ru/practicum/shareit/item/dto/CommentDto.java
@@ -4,16 +4,16 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
 import jakarta.validation.constraints.NotBlank;
-import ru.practicum.shareit.group.Marker;
 
+import ru.practicum.shareit.group.Marker;
 
 @AllArgsConstructor
 @Getter
 @Setter
 @NoArgsConstructor
 public class CommentDto {
+
     private Long id;
 
     @NotBlank(message = "Текст комментария отсутствует.", groups = {Marker.OnCreate.class})
